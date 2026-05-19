f535bfaae4e17faaecd569ff4bc8c48e50e96914
Meikou <121649289+Meikouz@users.noreply.github.com>
2026-05-19 10:21:53 +0300

    Add files via upload

---
+++ TextAnalyzer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LAB12
+{
+    public static class TextAnalyzer
+    {
+        public static int CountIntegers(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return 0;
+            }
+
+            string pattern = @"(?<![\p{L}\p{N}])(?<!\d[.,])[-+]?\d+(?![.,]\d)(?![\p{L}\p{N}])";
+
+            return Regex.Matches(text, pattern).Count;
+        }
+    }
+}
+
+// Метод для перевірки, до написання коду
+//{
+ //  public static class TextAnalyzer
+   // {
+     //   public static int CountIntegers(string text)
+       // {
+         //   throw new NotImplementedException();
+        //}
+    //}
+//}
